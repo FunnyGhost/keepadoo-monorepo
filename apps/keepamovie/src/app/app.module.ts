0e95a9b126be8600cbc8b0f8115a08d191f8694b
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-04-05 00:00:43 +0200

    Test

--- apps/keepamovie/src/app/app.module.ts
+++ apps/keepamovie/src/app/app.module.ts
@@ -38,7 +38,7 @@ const routes: Route[] = [
         loadChildren: () =>
           import('./movies-lists/movies-lists.module').then((m) => m.MoviesListsModule)
       },
-      // Until there is a home page, redirect to movies lists
+      // Until there is a home page, redirect to  movies lists
       {
         path: '',
         pathMatch: 'full',
