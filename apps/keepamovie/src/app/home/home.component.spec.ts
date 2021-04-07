0683efe44fa243cab771d901b6e19bbc5357ae19
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-04-07 23:07:25 +0200

    Cleanup HomeComponent + Test fixes

--- apps/keepamovie/src/app/home/home.component.spec.ts
+++ apps/keepamovie/src/app/home/home.component.spec.ts
@@ -1,76 +1,97 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { authServiceMock } from '../../test-utilities/test-mocks';
 import { AuthService } from '../state/auth.service';
 import { HomeComponent } from './home.component';
+import { getElementForTest } from '../../test-utilities/test-functions';
+import { SessionQuery } from '../state/session.query';
+import { ReplaySubject } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        imports: [RouterTestingModule],
-        providers: [
-          {
-            provide: AuthService,
-            useValue: authServiceMock
-          }
-        ],
-        declarations: [HomeComponent]
-      }).compileComponents();
-    })
-  );
+  const loggedInStream = new ReplaySubject<boolean>(1);
+  const sessionQueryMock: Partial<SessionQuery> = {
+    isLoggedIn$: loggedInStream.asObservable()
+  };
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: { signOut: () => '' }
+        },
+        {
+          provide: SessionQuery,
+          useValue: sessionQueryMock
+        }
+      ],
+      declarations: [HomeComponent]
+    });
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    loggedInStream.next(true);
   });
 
   test('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
 
-  test('should navigate to the add route when the + button is clicked', () => {
-    const router = TestBed.inject(Router);
-    jest.spyOn(router, 'navigateByUrl').mockImplementation();
+  test('should have a logo', () => {
+    fixture.detectChanges();
+    const logo = getElementForTest(fixture, 'logo');
 
-    const addButton = fixture.debugElement.query(By.css('.add-button'));
-    addButton.triggerEventHandler('click', null);
-
-    expect(router.navigateByUrl).toHaveBeenCalledWith(`${router.url}/add`);
+    expect(logo).toBeTruthy();
   });
 
-  test('should navigate to the add route when the + button is clicked', () => {
-    const authService = TestBed.inject(AuthService);
-    jest.spyOn(authService, 'signOut');
-    const signOutButton = fixture.debugElement.query(By.css('.sign-out-button'));
-    signOutButton.triggerEventHandler('click', null);
+  test('should have content', () => {
+    fixture.detectChanges();
+    const content = getElementForTest(fixture, 'content');
 
-    expect(authService.signOut).toHaveBeenCalled();
+    expect(content).toBeTruthy();
   });
 
-  describe('Render', () => {
-    test('should have a link for the movies feature', () => {
-      const movieFeatureLink = fixture.debugElement.queryAll(By.css('.movies-link'));
-
-      expect(movieFeatureLink.length).toBe(1);
+  describe('LoggedIn', () => {
+    beforeEach(() => {
+      loggedInStream.next(true);
     });
 
-    test('should have a link for the tv-shows feature', () => {
-      const movieFeatureLink = fixture.debugElement.queryAll(By.css('.tv-shows-link'));
+    test('should show the logout button', () => {
+      fixture.detectChanges();
+      const logoutButton = getElementForTest(fixture, 'logoutButton');
 
-      expect(movieFeatureLink.length).toBe(1);
+      expect(logoutButton).toBeTruthy();
     });
 
-    test('should have a link for the places feature', () => {
-      const movieFeatureLink = fixture.debugElement.queryAll(By.css('.places-link'));
+    test('should logout the user when the logout button is clicked', () => {
+      const authService = TestBed.inject(AuthService);
+      jest.spyOn(authService, 'signOut');
+      fixture.detectChanges();
+      const logoutButton = getElementForTest(fixture, 'logoutButton');
 
-      expect(movieFeatureLink.length).toBe(1);
+      logoutButton.triggerEventHandler('click', null);
+
+      expect(authService.signOut).toHaveBeenCalled();
+    });
+  });
+
+  describe('LoggedOut', () => {
+    beforeEach(() => {
+      loggedInStream.next(false);
+    });
+
+    test('should not show the logout button', () => {
+      fixture.detectChanges();
+      const logoutButton = getElementForTest(fixture, 'logoutButton');
+
+      expect(logoutButton).toBeFalsy();
     });
   });
 });
--- apps/keepamovie/src/app/home/home.component.ts
+++ apps/keepamovie/src/app/home/home.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService } from '../state/auth.service';
 import { SessionQuery } from '../state/session.query';
 
 @Component({
   selector: 'keepadoo-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
   isLoggedIn$ = this.sessionQuery.isLoggedIn$;
 
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private sessionQuery: SessionQuery
-  ) {}
-
-  goToAdd(): void {
-    this.router.navigateByUrl(`${this.router.url}/add`);
-  }
+  constructor(private authService: AuthService, private sessionQuery: SessionQuery) {}
 
   signOut(): void {
     this.authService.signOut();
