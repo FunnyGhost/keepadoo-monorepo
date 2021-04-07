3bb1e08d4ad74b137b29f9233fae91188c593d60
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-04-07 22:20:41 +0200

    Added function to get element by dataTestName

--- apps/keepamovie/src/test-utilities/test-functions.ts
+++ apps/keepamovie/src/test-utilities/test-functions.ts
@@ -1,7 +1,14 @@
-import { Type } from '@angular/core';
+import { DebugElement, Type } from '@angular/core';
 import { ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 export function childComponents<T>(fixture: ComponentFixture<any>, type: Type<T>): T[] {
   return fixture.debugElement.queryAll(By.directive(type)).map((el) => el.componentInstance);
+}
+
+export function getElementForTest(
+  fixture: ComponentFixture<any>,
+  dataTestName: string
+): DebugElement {
+  return fixture.debugElement.query(By.css(`[data-test-name=${dataTestName}`));
 }
