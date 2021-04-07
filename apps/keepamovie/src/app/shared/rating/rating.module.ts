167a39bef9526184ffcece9c5b6151fdc0f1694f
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-04-07 16:48:01 +0200

    Created a rating pipe

---
+++ apps/keepamovie/src/app/shared/rating/rating.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RatingPipe } from './rating.pipe';
+
+@NgModule({
+  declarations: [RatingPipe],
+  imports: [CommonModule],
+  exports: [RatingPipe]
+})
+export class RatingModule {}
