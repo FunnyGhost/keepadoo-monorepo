ad195450b9bf54785d76ac09e85f9ac623488d46
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-04-08 15:20:07 +0200

    Cleanup MoviesListCreate Component + Test fix

--- apps/keepamovie/src/app/movies-lists/movies-list-create/movies-list-create.component.ts
+++ apps/keepamovie/src/app/movies-lists/movies-list-create/movies-list-create.component.ts
@@ -5,7 +5,6 @@ import { MoviesListsQuery } from '../state/movies-lists.query';
 import { MoviesListsService } from '../state/movies-lists.service';
 
 @Component({
-  selector: 'keepadoo-movies-list-create',
   templateUrl: './movies-list-create.component.html',
   styleUrls: ['./movies-list-create.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -31,8 +30,8 @@ export class MoviesListCreateComponent implements OnInit {
     this.loading$ = this.query.selectLoading();
   }
 
-  async onSubmit() {
-    await this.moviesListService.add({ name: this.moviesListForm.value.name });
+  onSubmit() {
+    this.moviesListService.add({ name: this.moviesListForm.value.name });
     this.onClose();
   }
 
