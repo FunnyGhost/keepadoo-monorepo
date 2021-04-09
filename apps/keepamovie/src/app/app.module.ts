a53f9b2c453d7ae83488ccdd00e97eb77bf791dd
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-04-09 15:14:01 +0200

    Added logout icon

--- apps/keepamovie/src/app/app.module.ts
+++ apps/keepamovie/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { SvgIconsModule } from '@ngneat/svg-icon';
 import { appPlusIcon } from './svg/plus';
 import { appCloseIcon } from './svg/close';
 import { appStarIcon } from '@app/svg/star';
+import { appLogoutIcon } from '@app/svg/logout';
 
 const routes: Route[] = [
   {
@@ -71,7 +72,7 @@ const routes: Route[] = [
     }),
     HotToastModule.forRoot(),
     SvgIconsModule.forRoot({
-      icons: [appPlusIcon, appCloseIcon, appStarIcon]
+      icons: [appPlusIcon, appCloseIcon, appStarIcon, appLogoutIcon]
     })
   ],
   bootstrap: [AppComponent]
