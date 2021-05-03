8e0a20a17e19448a6e04133e27d610bc01f3f198
Catalin Ciubotaru <6450559+FunnyGhost@users.noreply.github.com>
2021-05-03 12:23:47 +0200

    Created reusable slide-up component

---
+++ apps/keepamovie/src/app/shared/slide-up/slide-up.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SlideUpComponent } from './slide-up.component';
+
+describe('SlideUpComponent', () => {
+  let component: SlideUpComponent;
+  let fixture: ComponentFixture<SlideUpComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SlideUpComponent]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SlideUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/keepamovie/src/app/shared/slide-up/slide-up.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SlideUpComponent } from './slide-up.component';
+
+
+
+@NgModule({
+  declarations: [
+    SlideUpComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SlideUpComponent
+  ]
+})
+export class SlideUpModule { }
